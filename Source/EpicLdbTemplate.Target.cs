8092e5427fe0cf624ee99e9638bd2d631eeed4db
Jens Roth <98889125+jensroth-git@users.noreply.github.com>
2026-02-10 17:10:10 +0100

    Upgrade to Unreal Engine 5.7 and update build settings to use version 6 and IncludeOrderVersion Unreal5_7

--- Source/EpicLdbTemplate.Target.cs
+++ Source/EpicLdbTemplate.Target.cs
@@ -8,7 +8,8 @@ public class EpicLdbTemplateTarget : TargetRules
 	public EpicLdbTemplateTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		ExtraModuleNames.AddRange( new string[] { "EpicLdbTemplate" } );
 	}
--- Source/EpicLdbTemplateEditor.Target.cs
+++ Source/EpicLdbTemplateEditor.Target.cs
@@ -8,7 +8,8 @@ public class EpicLdbTemplateEditorTarget : TargetRules
 	public EpicLdbTemplateEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		ExtraModuleNames.AddRange( new string[] { "EpicLdbTemplate" } );
 	}
